f57b810ab1dce9afb9e0277152f03f3c5be24d36
ramazan <sahinkartal863s@gmail.com>
2023-11-24 09:33:50 +0300

    24 KASIM EKLENDI

---
+++ src/J19_Constructor/C05_AracRunner.java
@@ -0,0 +1,13 @@
+package J19_Constructor;
+
+public class C05_AracRunner {//runner Class level
+    public static void main(String[] args) {//main level
+        C05_Arac a1=new C05_Arac("hacı volvo");
+        System.out.println("a1 = " + a1);//a1 = model='hacı volvo', maxHiz=124
+        a1.maxHiz=210;
+        System.out.println("a1 = " + a1);// a1 = model='hacı volvo', maxHiz=210
+
+
+    }//main sonu
+
+}//Class sonu
